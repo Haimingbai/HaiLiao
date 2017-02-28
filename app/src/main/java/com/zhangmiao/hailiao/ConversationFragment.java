c6a3db9b731493f7d48c5f3228ecf8e3ad488203
unknown <zhangmiao@meizu.com>
2017-02-28 15:37:56 +0800

    1.添加聊天中的图片发送与接受。

--- app/src/main/java/com/zhangmiao/hailiao/ConversationFragment.java
+++ app/src/main/java/com/zhangmiao/hailiao/ConversationFragment.java
@@ -7,18 +7,33 @@ import android.text.style.TtsSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMConversation;
 import com.zhangmiao.hailiao.R;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by zhangmiao on 2017/2/24.
  */
 public class ConversationFragment extends Fragment {
 
+    private ListView mConversationListView;
+    private Map<String, EMConversation> mConversations;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.conversation, container, false);
+
+        mConversationListView = (ListView) view.findViewById(R.id.conversation_list);
+        mConversations = EMClient.getInstance().chatManager().getAllConversations();
+
+
         return view;
     }
 }
--- app/src/main/java/com/zhangmiao/hailiao/UI/ChatActivity.java
+++ app/src/main/java/com/zhangmiao/hailiao/UI/ChatActivity.java
@@ -1,25 +1,45 @@
 package com.zhangmiao.hailiao.UI;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.design.widget.BottomSheetDialog;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.hyphenate.EMMessageListener;
 import com.hyphenate.chat.EMClient;
 import com.hyphenate.chat.EMConversation;
+import com.hyphenate.chat.EMFileMessageBody;
+import com.hyphenate.chat.EMImageMessageBody;
 import com.hyphenate.chat.EMMessage;
+import com.hyphenate.chat.EMTextMessageBody;
+import com.hyphenate.easeui.EaseConstant;
+import com.hyphenate.easeui.model.EaseImageCache;
+import com.hyphenate.easeui.utils.EaseCommonUtils;
+import com.hyphenate.easeui.utils.EaseImageUtils;
 import com.zhangmiao.hailiao.R;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,8 +52,10 @@ public class ChatActivity extends Activity {
     private String friendName;
     List<EMMessage> messages;
     List<String> EMMessageUserName;
-    List<String> EMMessageText;
+    List<String> EMMessageBody;
+    List<EMMessage> emMessages;
     ChatMessageListAdapter adapter;
+    private Button mChatSendPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,8 +73,12 @@ public class ChatActivity extends Activity {
         mFriendName.setText(friendName);
         mChatSendButton.setOnClickListener(sendListener);
 
+        mChatSendPhoto = (Button) findViewById(R.id.chat_send_photo);
+        mChatSendPhoto.setOnClickListener(sendPhotoListener);
+
         EMMessageUserName = new ArrayList<>();
-        EMMessageText = new ArrayList<>();
+        EMMessageBody = new ArrayList<>();
+        emMessages = new ArrayList<>();
 
         EMClient.getInstance().chatManager().addMessageListener(messageListener);
 
@@ -67,8 +93,8 @@ public class ChatActivity extends Activity {
                 EMMessageUserName.add(emMessage.getFrom());
 
                 String text = emMessage.getBody().toString();
-                String[] subText = text.split(":");
-                EMMessageText.add(subText[1].substring(1, subText[1].length() - 1));
+                EMMessageBody.add(text);
+                emMessages.add(emMessage);
             }
         }
         adapter = new ChatMessageListAdapter(this);
@@ -82,9 +108,37 @@ public class ChatActivity extends Activity {
             EMMessage message = EMMessage.createTxtSendMessage(content, friendName);
             EMClient.getInstance().chatManager().sendMessage(message);
             mChatSendMessage.setText("");
-            adapter.addData(EMClient.getInstance().getCurrentUser(), content);
+            //adapter.addData(EMClient.getInstance().getCurrentUser(), content);
+            adapter.addData(EMClient.getInstance().getCurrentUser(), message);
         }
     };
+
+    private View.OnClickListener sendPhotoListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent picture = new Intent(Intent.ACTION_PICK,
+                    android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(picture, 3);
+        }
+    };
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            Uri uri = data.getData();
+            String[] proj = {MediaStore.Images.Media.DATA};
+            Cursor cursor = managedQuery(uri, proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToNext();
+
+            String path = cursor.getString(column_index);
+            EMMessage message = EMMessage.createImageSendMessage(path, false, friendName);
+            EMClient.getInstance().chatManager().sendMessage(message);
+            //adapter.addData(EMClient.getInstance().getCurrentUser(), message.getBody().toString());
+            adapter.addData(EMClient.getInstance().getCurrentUser(), message);
+        }
+    }
 
     private EMMessageListener messageListener = new EMMessageListener() {
         @Override
@@ -92,10 +146,9 @@ public class ChatActivity extends Activity {
             Log.e("test", "收到消息");
             for (int i = 0; i < list.size(); i++) {
                 EMMessage emMessage = list.get(i);
-                String text = emMessage.getBody().toString();
-                String[] subText = text.split(":");
-
-                adapter.addData(emMessage.getFrom(), subText[1].substring(1, subText[1].length() - 1));
+                String body = emMessage.getBody().toString();
+                //adapter.addData(emMessage.getFrom(), body);
+                adapter.addData(emMessage.getFrom(), emMessage);
             }
         }
 
@@ -157,16 +210,117 @@ public class ChatActivity extends Activity {
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             TextView userName;
-            TextView conversation;
+            TextView conversation_text;
+            ImageView conversation_image;
             if (convertView == null) {
                 convertView = LayoutInflater.from(mContext).inflate(R.layout.chat_message_item, null);
             }
             userName = (TextView) convertView.findViewById(R.id.message_item_user_name);
-            conversation = (TextView) convertView.findViewById(R.id.message_item_conversation);
+            conversation_text = (TextView) convertView.findViewById(R.id.message_item_conversation_text);
+            conversation_image = (ImageView) convertView.findViewById(R.id.message_item_conversation_image);
             userName.setText(EMMessageUserName.get(position));
-            conversation.setText(EMMessageText.get(position));
 
+            final EMMessage msg = emMessages.get(position);
+            EMMessage.Type type = msg.getType();
+            switch (type) {
+                case TXT:
+                    String content = ((EMTextMessageBody) msg.getBody()).getMessage();
+                    conversation_text.setText(content);
+                    break;
+                case IMAGE:
+                    conversation_text.setVisibility(View.INVISIBLE);
+                    conversation_image.setVisibility(View.VISIBLE);
+                    EMImageMessageBody body = (EMImageMessageBody) msg.getBody();
+                    if (new File(body.getLocalUrl()).exists()) {
+                        Bitmap bitmap = BitmapFactory.decodeFile(body.getLocalUrl());
+                        int width = bitmap.getWidth();
+                        int height = bitmap.getHeight();
+                        BitmapFactory.Options options = new BitmapFactory.Options();
+                        if (height > 160) {
+                            options.inSampleSize = width > height ? width / 160 : height / 160;
+                        } else {
+                            options.inSampleSize = 1;
+                        }
+                        conversation_image.setImageBitmap(BitmapFactory.decodeFile(body.getLocalUrl(), options));
+                        break;
+                    }
+                    if (msg.direct() == EMMessage.Direct.RECEIVE) {
+                        if (body.thumbnailDownloadStatus() ==
+                                EMFileMessageBody.EMDownloadStatus.DOWNLOADING
+                                || body.thumbnailDownloadStatus() ==
+                                EMFileMessageBody.EMDownloadStatus.PENDING
+                                ) {
+                            Log.e("test", "if ...");
+                        } else {
+                            String thumbPath = body.thumbnailLocalPath();
+                            if (!new File(thumbPath).exists()) {
+                                thumbPath = EaseImageUtils.getThumbnailImagePath(body.getLocalUrl());
+                            }
+                            showImageView(body, thumbPath, conversation_image, body.getLocalUrl(), msg);
+                        }
+                        String filePath = body.getLocalUrl();
+                        String thumbPath = EaseImageUtils.getThumbnailImagePath(body.getLocalUrl());
+
+                        showImageView(body, thumbPath, conversation_image, filePath, msg);
+                    }
+                    break;
+                default:
+                    String defaultMessage = ((EMTextMessageBody) msg.getBody()).getMessage();
+                    conversation_text.setText(defaultMessage);
+                    break;
+            }
             return convertView;
+        }
+
+        private boolean showImageView(final EMImageMessageBody body, final String thumbernailPath, final ImageView iv, final String localFullSizePath, final EMMessage message) {
+            Bitmap bitmap = EaseImageCache.getInstance().get(thumbernailPath);
+            if (bitmap != null) {
+                iv.setImageBitmap(bitmap);
+                return true;
+            } else {
+                new AsyncTask<Object, Void, Bitmap>() {
+
+                    @Override
+                    protected Bitmap doInBackground(Object... params) {
+                        File file = new File(thumbernailPath);
+                        if (file.exists()) {
+                            return EaseImageUtils.decodeScaleImage(thumbernailPath, 160, 160);
+                        } else if (new File(body.thumbnailLocalPath()).exists()) {
+                            return EaseImageUtils.decodeScaleImage(body.thumbnailLocalPath(), 160, 160);
+                        } else {
+                            if (message.direct() == EMMessage.Direct.SEND) {
+                                if (localFullSizePath != null && new File(localFullSizePath).exists()) {
+                                    return EaseImageUtils.decodeScaleImage(localFullSizePath, 160, 160);
+                                } else {
+                                    return null;
+                                }
+                            } else {
+                                return null;
+                            }
+                        }
+                    }
+
+                    @Override
+                    protected void onPostExecute(Bitmap bitmap) {
+                        if (bitmap != null) {
+                            iv.setImageBitmap(bitmap);
+                            EaseImageCache.getInstance().put(thumbernailPath, bitmap);
+                        } else {
+                            if (message.status() == EMMessage.Status.FAIL) {
+                                if (EaseCommonUtils.isNetWorkConnected(ChatActivity.this)) {
+                                    new Thread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            EMClient.getInstance().chatManager().downloadThumbnail(message);
+                                        }
+                                    }).start();
+                                }
+                            }
+                        }
+                    }
+                }.execute();
+            }
+            return true;
         }
 
         public void addData(String username, String message) {
@@ -174,12 +328,25 @@ public class ChatActivity extends Activity {
                 EMMessageUserName = new ArrayList<>();
             }
             EMMessageUserName.add(username);
-            if (EMMessageText == null) {
-                EMMessageText = new ArrayList<>();
+            if (EMMessageBody == null) {
+                EMMessageBody = new ArrayList<>();
             }
-            EMMessageText.add(message);
+            EMMessageBody.add(message);
             notifyDataSetChanged();
         }
+
+        public void addData(String username, EMMessage message) {
+            if (EMMessageUserName == null) {
+                EMMessageUserName = new ArrayList<>();
+            }
+            EMMessageUserName.add(username);
+            if (EMMessageBody == null) {
+                EMMessageBody = new ArrayList<>();
+            }
+            emMessages.add(message);
+            notifyDataSetChanged();
+        }
+
     }
 
 }
